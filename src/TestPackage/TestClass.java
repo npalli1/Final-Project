08783cb25d0e7bc12fe133e37fba03afcbb8ee62
Nikhil Palli <npalli1@student.gsu.edu>
2017-11-13 21:23:20 -0500

    Word.

--- src/TestPackage/TestClass.java
+++ src/TestPackage/TestClass.java
@@ -4,8 +4,16 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Check.");
+		int a = 1;
+		int b = 20;
+		
+		printSum(a,b);
 
+	}
+	
+	public static void printSum(int a, int b){
+		
+		System.out.println("The sum is " + (a + b) );
 	}
 
 }
