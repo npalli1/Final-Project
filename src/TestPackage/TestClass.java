84e53e77b490cdb2001a61e0ac2b2c2ca49b2ae6
Nikhil Palli <npalli1@student.gsu.edu>
2017-11-14 17:35:42 -0500

    i hope this works sean.

--- src/TestPackage/TestClass.java
+++ src/TestPackage/TestClass.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 public class TestClass {
 
 	public static void main(String[] args){
+
 	    Gui splash = new Gui();
 	    splash.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    splash.setSize(300,200);
